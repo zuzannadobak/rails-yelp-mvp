2f65af1ff28b2bd6ab83983f797d35cb8db23590
zuzannadobak <mail@zuzannadobak.com>
2023-08-16 16:37:08 +0200

    create button and submit form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,29 +1,35 @@
 class RestaurantsController < ApplicationController
 # GET "restaurants"
-def index
-  @restaurants = Restaurant.all
+  def index
+    @restaurants = Restaurant.all
 
-end
-# GET "restaurants/new"
-# POST "restaurants"
-# GET "restaurants/38"
-
-# GET "restaurants/38/reviews/new"
-# POST "restaurants/38/reviews"
-def show
-  @restaurant = Restaurant.find(params[:id])
-end
-
-def new
-  @restaurant = Restaurant.new
-end
-
-def create
-  @restaurant = Restaurant.new(restaurant_params)
-  if @restaurant.save
-    redirect_to @restaurant, notice: "Restaurant was successfully created."
-  else
-    render :new, status: :unprocessable_entity
   end
-end
+  # GET "restaurants/new"
+  # POST "restaurants"
+  # GET "restaurants/38"
+
+  # GET "restaurants/38/reviews/new"
+  # POST "restaurants/38/reviews"
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant, notice: "Restaurant was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :category, :address, :phone_number)
+  end
 end
